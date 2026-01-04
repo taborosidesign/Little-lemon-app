5093d92494148fdbcaddc60b25e3c50da354cd8e
ataborosi-home <info@taborosidesign.com>
2026-01-04 17:34:10 +0100

    react-router-dom installed and attached for the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-   
-    </>
+    <Routes>
+      <Route path="/" element={<h1>Hello</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+    </Routes>
   )
 }
 
